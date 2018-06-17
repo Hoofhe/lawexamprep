0098f13406afcb89ec75aef6c35c0c9c68fa4b50
Amrinder Singh Kalsi <amrinder.kalsi13@gmail.com>
2018-06-16 21:15:06 -0400

    Configured Hibernate and added MVC layers to the backend project.

--- lawexamprep/src/main/java/com/lawexamprep/controller/PageController.java
+++ lawexamprep/src/main/java/com/lawexamprep/controller/PageController.java
@@ -1,4 +1,4 @@
-package com.hoofhe.lawexamprep.controller;
+package com.lawexamprep.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ lawexamprepbackend/database.sql
@@ -0,0 +1,105 @@
+CREATE DATABASE lawexamprep_db;
+USE lawexamprep_db;
+
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  role VARCHAR(50),
+  isactive tinyint(1) NOT NULL DEFAULT 1,
+  lastlogindate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  createdby int(11) DEFAULT NULL,
+  modifiedby int(11) DEFAULT NULL,
+  passwordattemp int(11) DEFAULT '0',
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE userprofile (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  userid int(11) NOT NULL,
+  firstname varchar(255) NOT NULL,
+  middlename varchar(255) NOT NULL,
+  lastname varchar(255) NOT NULL,
+  addressline1 varchar(255) NOT NULL,
+  addressline2 varchar(255) NOT NULL,
+  city varchar(255) NOT NULL,
+  province varchar(255) NOT NULL,
+  postalcode varchar(255) NOT NULL,
+  country varchar(255) NOT NULL,
+  PRIMARY KEY (id),
+  KEY userid (userid),
+  CONSTRAINT userprofile_ibfk_1 FOREIGN KEY (userid) REFERENCES users (id)
+);
+
+CREATE TABLE papertype (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  typename varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE papersubtype (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  papertypeid int(11) NOT NULL,
+  subtypename varchar(255) NOT NULL,
+  PRIMARY KEY (id),
+  KEY papertypeid (papertypeid),
+  CONSTRAINT papersubtype_ibfk_1 FOREIGN KEY (papertypeid) REFERENCES papertype (id)
+);
+
+CREATE TABLE questions (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  papertypeid int(11) NOT NULL,
+  papersubtypeid int(11) NOT NULL,
+  questiontext text NOT NULL,
+  PRIMARY KEY (id),
+  KEY papertypeid (papertypeid),
+  KEY papersubtypeid (papersubtypeid),
+  CONSTRAINT questions_ibfk_1 FOREIGN KEY (papertypeid) REFERENCES papertype (id),
+  CONSTRAINT questions_ibfk_2 FOREIGN KEY (papersubtypeid) REFERENCES papersubtype (id)
+);
+
+CREATE TABLE answers (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  questionid int(11) NOT NULL,
+  answertext varchar(255) NOT NULL,
+  isCorrect tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id),
+  KEY questionid (questionid),
+  CONSTRAINT answers_ibfk_1 FOREIGN KEY (questionid) REFERENCES questions (id)
+);
+
+CREATE TABLE userprogress (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  userid int(11) NOT NULL,
+  questionid int(11) NOT NULL,
+  answerid int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY answerid (answerid),
+  KEY questionid (questionid),
+  KEY userid (userid),
+  CONSTRAINT userprogress_ibfk_1 FOREIGN KEY (answerid) REFERENCES answers (id),
+  CONSTRAINT userprogress_ibfk_2 FOREIGN KEY (questionid) REFERENCES questions (id),
+  CONSTRAINT userprogress_ibfk_3 FOREIGN KEY (userid) REFERENCES users (id)
+);
+
+CREATE TABLE userpapertype (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  userid int(11) NOT NULL,
+  papertypeid int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY papertypeid (papertypeid),
+  KEY userid (userid),
+  CONSTRAINT userpapertype_ibfk_1 FOREIGN KEY (papertypeid) REFERENCES papertype (id),
+  CONSTRAINT userpapertype_ibfk_2 FOREIGN KEY (userid) REFERENCES users (id)
+);
+
+CREATE TABLE userattempt (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  userid int(11) NOT NULL,
+  attemptno int(11) NOT NULL,
+  completed tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id),
+  KEY userid (userid),
+  CONSTRAINT userattempt_ibfk_1 FOREIGN KEY (userid) REFERENCES users (id)
+);
+
